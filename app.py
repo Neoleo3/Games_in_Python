74c94bf7a5cc3bfaa162d1bf56abdd5a45fbfdee
Sathyajit G A <gasathyajit@gmail.com>
2025-02-23 15:49:09 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,36 +1,34 @@
 import streamlit as st
-from November2024_C3_S10_PracticeSolution import Guessing_Game,Riddle_puzzle,Arithmetic_puzzle,General_Knowledge,Rock_paper_scissor,Rock_Paper_scissor_game,main1,main2,main3,main4,main5,main6
+from November2024_C3_S10_PracticeSolution import (
+    Guessing_Game, Riddle_puzzle, Arithmetic_puzzle, General_Knowledge,
+    Rock_paper_scissor, Rock_Paper_scissor_game, main1, main2, main3, main4, main5, main6
+)
 
-st.header('Welcome to a Catalog of games, fellow User!')
-def main():
-    st.write('Pick a game of your choice and play on!')
-    valid_choices = [0,1,2,3,4,5,6,7]
-    end_game =0
-    while end_game==0:
-        try:
-            user_num=st.number_input('Game code: 1 is Riddle Puzzle, 2 is Arithmatic Puzzle, 3 is General Knowledge Quiz Game, 4 is Rock Paper Scissor with computer,5 is a Rock Paper Scissor game with a User,6 is Word Guessing game,7 to exit the game',min_value=0,max_value=7)
-            if user_num not in valid_choices:
-                print('You have entered the wrong option')
-                continue
-        except ValueError as e:
-            print(f'{e}')
-        else:
-            if user_num==1:
-                main1()
-            elif user_num==2:
-                main2()
-            elif user_num==3:
-                main3()
-            elif user_num==4:
-                main4()
-            elif user_num==5:
-                main5()
-            elif user_num==6:
-                main6()
-            else:
-                print('Thank you! GoodBye')
-                break 
+st.header("🎮 Welcome to a Catalog of Games!")
 
-if st.button('Play Game'):
-    main() 
+st.write("Pick a game of your choice and play on!")
+
+# Create a dropdown instead of number input
+game_choice = st.selectbox(
+    "Select a game to play:",
+    ["Select a game", "Riddle Puzzle", "Arithmetic Puzzle", "General Knowledge Quiz",
+     "Rock Paper Scissor (vs Computer)", "Rock Paper Scissor (vs User)", "Word Guessing Game", "Exit"]
+)
+
+# Create a button to confirm game selection
+if st.button("Start Game"):
+    if game_choice == "Riddle Puzzle":
+        main1()
+    elif game_choice == "Arithmetic Puzzle":
+        main2()
+    elif game_choice == "General Knowledge Quiz":
+        main3()
+    elif game_choice == "Rock Paper Scissor (vs Computer)":
+        main4()
+    elif game_choice == "Rock Paper Scissor (vs User)":
+        main5()
+    elif game_choice == "Word Guessing Game":
+        main6()
+    elif game_choice == "Exit":
+        st.write("🎮 Thank you! See you next time!")
 
